ed82b7b8c88febb91c2c5b341bb6dd2c971cc845
ainchwarom <ainchwaro@gmail.com>
2026-07-25 11:40:32 +0530

    Update frontend to use deployed Render API

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "http://127.0.0.1:8002";
+  const API_BASE = "https://brain-check-score.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
